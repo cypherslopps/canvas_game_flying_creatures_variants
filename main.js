4151e1c03c3e9bc7d76070241441338105c62e46
Joseph Etim Ibok <josephibok36@gmail.com>
2024-06-01 16:48:28 +0100

    Flying Creature Variant #4

--- main.js
+++ main.js
@@ -3,26 +3,26 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = 500;
 canvas.height = 1000
-const numberOfEnemies = 50;
+const numberOfEnemies = 20;
 const enemiesArray = [];
 let gameFrame = 0;
 
 class Enemy {
     constructor() {
         this.image = new Image();
-        this.image.src = "/images/enemy3.png";
+        this.image.src = "/images/enemy4.png";
         this.speed = Math.random() * 4 - 1;
-        this.spriteWidth = 218;
-        this.spriteHeight = 177;
-        this.width = this.spriteWidth / 2.5;
-        this.height = this.spriteHeight / 2.5;
+        this.spriteWidth = 213;
+        this.spriteHeight = 212;
+        this.width = this.spriteWidth / 2;
+        this.height = this.spriteHeight / 2;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;  
+        this.newX = Math.random() * (canvas.width - this.width);  
+        this.newY = Math.random() * (canvas.height - this.height);
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-        this.angle = 0;
-        this.angleSpeed = Math.random() * 0.5 + 0.5;
-        this.curve = Math.random() * 200 + 50;
+        this.interval = Math.floor(Math.random() * 200 + 50)
     }
 
     draw() {
@@ -31,10 +31,18 @@ class Enemy {
 
     update() {
         this.draw();
-        this.x = canvas.width / 2 * Math.sin(this.angle * Math.PI/90) + (canvas.width / 2) - (this.width / 2);
-        this.y = canvas.height / 2 * Math.cos(this.angle * Math.PI/700) + (canvas.height / 2) - (this.height / 2);
-        this.angle += this.angleSpeed;
 
+        if (gameFrame % this.interval === 0) {
+            this.newX = Math.random() * (canvas.width - this.width);  
+            this.newY = Math.random() * (canvas.height - this.height);
+        }
+        // this.x = 0;
+        // this.y = 0;
+        let dx = this.x - this.newX;
+        let dy = this.y - this.newY;
+        this.x -= dx/70;
+        this.y -= dy/70;
+        
         // if (this.y >= canvas.height) {
         //     if (this.y >= canvas.height / 2) {
         //         this.y += Math.sin(this.angle) * this.curve;
@@ -47,7 +55,7 @@ class Enemy {
 
         // Animate sprites
         if (gameFrame % this.flapSpeed === 0) {
-            this.frame > 4 ? this.frame = 0 : this.frame++;
+            this.frame > 7 ? this.frame = 0 : this.frame++;
         }
     }
 }
